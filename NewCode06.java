0e597b111e8f2b82575df4f644bca7d1e86953f9
Abhay734 <abhayrajtiwari734@gmail.com>
2024-09-26 11:50:20 +0530

    code on conditinal operators

---
+++ NewCode06.java
@@ -0,0 +1,14 @@
+
+public class NewCode06{
+
+    public static void main(String args[]){
+        int a = 2;
+        int b = 5;
+        if(a>=b ){
+            System.out.println("a is largest of 2");
+            
+        }else {
+        System.out.println("b is largest of 2");
+        }
+    }
+}
